7b6b85ddf0c9a3ad5852c9eb5a18fccc3f529779
Mitch Dominici <dominici.mitchell@gmail.com>
2018-10-10 10:11:10 -0500

    working on single blog

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     title = db.Column(db.String(120))
-    body = db.Column(db.String(500))
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(1000))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title,body, owner):   
+    def __init__(self, title,body, user):   
         self.title = title
         self.body = body     
-        self.owner = owner
+        self.user = user
 
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     username =  db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))    
-    blogs= db.relationship('Blog', backref ='owner' )
+    blogs = db.relationship('Blog', backref='user')
 
     def __init__(self, username,password):
         self.username = username
@@ -33,14 +33,28 @@ class User(db.Model):
 
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'signup', 'blog']
-    if request.endpoint not in allowed_routes and 'username'not in session:
-        return redirect('/login')
+    allowed_routes = ["signup", 'login', 'blog', 'index']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('login')
 
 @app.route('/logout')
 def logout():
     del session['username']
-    return redirect('/blog')
+    return redirect('/login')
+
+@app.route('/', methods=['POST','GET'])
+def index():
+
+    users= User.query.all()
+    username = request.args.get('user.id')
+    if username == None:
+        return render_template('index.html', users=users)
+
+    user= User.query.get(username)
+    return render_template('singleuser.html', user=user)
+
+
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -57,11 +71,8 @@ def login():
 
     return render_template('login.html')
 
-
-
-
 @app.route('/signup', methods=['POST','GET'])
-def user_signup():
+def signup():
     if request.method == 'POST':
         username=request.form['username']
         password_error=''
@@ -84,44 +95,37 @@ def user_signup():
             flash('Username already exists.', 'error')
     
     return render_template('signup.html')
-
-
-
-        
+      
 @app.route('/blog',methods = ['POST', 'GET'])
-def home_page():
+def blog():
 
     blogs = Blog.query.all()
-    blog_id= request.args.get('id')
+    blog_id= request.args.get('blog.id')
+    user_id = request.args.get('user.username')
     
     if blog_id == None:
         return render_template('blog.html', 
-        title="Build a Blog!", blogs=blogs )
-        
-    blog = Blog.query.get(blog_id)
-        
-    return render_template('single_blog.html'
-        ,blog=blog)
+        title="Blogz!!", blogs=blogs)
 
-
+    
+    else:
+        blog = Blog.query.get(blog_id)
+        user = User.query.get(user_id)
+        return render_template('single_blog.html'
+        ,blog=blog,user=user)
 
 @app.route('/newpost',methods = ['POST', 'GET'])
-def add_blog():
-
+def newpost():
+    user = User.query.filter_by(username=session['username']).first()
     if request.method == 'POST':
 
         title = request.form['title']
         body = request.form['body']
-        owner = request.args.get('User.id')
-        new_blog = Blog(title,body,owner)
-        
+        new_blog = Blog(title,body,user)
         db.session.add(new_blog)
         db.session.commit()
-        
         blog = Blog.query.order_by('-Blog.id').first()
-        
-
-        return render_template('single_blog.html',blog=blog, owner=owner)
+        return render_template('single_blog.html',blog=blog)
 
     else:
         return render_template('newpost.html',title="Add a Blog Post")
